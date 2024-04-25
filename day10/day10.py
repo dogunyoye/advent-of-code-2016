9bcf3955077ff44fd661f13b68e0bbe44416dc55
Dimeji Ogunyoye <dimeji.ogunyoye@diffusiondata.com>
2024-04-25 22:49:30 +0100

    Day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,5 +1,6 @@
 import os.path
 import re
+from math import prod
 
 DATA = os.path.join(os.path.dirname(__file__), 'day10.txt')
 
@@ -27,14 +28,60 @@ def __initialise_bots(data) -> dict:
 
 
 def find_bot_id_which_compares_61_and_17(data) -> int:
-    print(__initialise_bots(data))
-    return 0
+    bots, outputs = __initialise_bots(data), {}
+    target = (17, 61)
+
+    while True:
+        for k, v in bots.items():
+            values = v[0]
+            if len(values) == 2:
+                values.sort()
+                if (values[0], values[1]) == target:
+                    return k
+
+                for i, dest in enumerate([v[1], v[2]]):
+                    bot_or_output_id = int(dest.split(" ")[1])
+                    if dest.startswith("bot"):
+                        bots[bot_or_output_id][0].append(values[i])
+                    else:
+                        if bot_or_output_id not in outputs.keys():
+                            outputs[bot_or_output_id] = [values[i]]
+                        else:
+                            outputs[bot_or_output_id].append(values[i])
+
+                values.clear()
+
+
+def find_product_of_outputs_with_one_chip(data) -> int:
+    bots, outputs = __initialise_bots(data), {0: [], 1: [], 2: []}
+
+    while True:
+        for k, v in bots.items():
+            values = v[0]
+            if len(values) == 2:
+                values.sort()
+
+                for i, dest in enumerate([v[1], v[2]]):
+                    bot_or_output_id = int(dest.split(" ")[1])
+                    if dest.startswith("bot"):
+                        bots[bot_or_output_id][0].append(values[i])
+                    else:
+                        if bot_or_output_id not in outputs.keys():
+                            outputs[bot_or_output_id] = [values[i]]
+                        else:
+                            outputs[bot_or_output_id].append(values[i])
+
+                if len(outputs[0]) == 1 and len(outputs[1]) == 1 and len(outputs[2]) == 1:
+                    return prod([outputs[0][0], outputs[1][0], outputs[2][0]])
+
+                values.clear()
 
 
 def main() -> int:
     with open(DATA) as f:
         data = f.read()
         print("Part 1: " + str(find_bot_id_which_compares_61_and_17(data)))
+        print("Part 2: " + str(find_product_of_outputs_with_one_chip(data)))
     return 0
 
 
