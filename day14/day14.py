bb09541e112f0109257f856670860559f2fec3c7
Dimeji <dimejiogunyoye@googlemail.com>
2024-05-01 03:12:29 +0100

    Day 14 - part 1

--- day14/day14.py
+++ day14/day14.py
@@ -1,12 +1,41 @@
+import hashlib
 import os.path
 
 DATA = os.path.join(os.path.dirname(__file__), 'day14.txt')
 
 
 def find_index_that_produces_64th_key(data) -> int:
-    salt = int(data.splitlines()[0])
-    index = 0
-    return 0
+    salt, index = data.splitlines()[0], 0
+    pending, completed = [], []
+    found = set()
+
+    while True:
+        candidate = hashlib.md5((salt + str(index)).encode()).hexdigest()
+        to_remove = []
+
+        for i, p in enumerate(pending):
+            if p[2] == index and p[0] in found:
+                completed.append(int(p[0]))  # int conversion to stop PyCharm complaining.
+                to_remove.append(i)
+            elif p[1] in candidate:
+                if p[0] in found:
+                    to_remove.append(i)
+                else:
+                    found.add(int(p[0]))  # int conversion to stop PyCharm complaining.
+
+        if len(completed) == 64:
+            return completed[63]
+
+        to_remove.reverse()
+        for r in to_remove:
+            del pending[r]
+
+        for i in range(0, len(candidate) - 2):
+            if candidate[i] == candidate[i + 1] == candidate[i + 2]:
+                pending.append((index, candidate[i] * 5, 1000 + index))
+                break
+
+        index += 1
 
 
 def main() -> int:
